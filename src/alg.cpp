f23d7d91caaaff350337ab20ecf6b6397ee7588e
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-05-26 12:03:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,56 +5,53 @@
 
 double pown(double value, uint16_t n) {
 
-   double i = value;
-   if (n == 0) {
-     return 1;
-   }
-   if (n == 1) {
-     return value;
-   }
-   i *= pown(value, n - 1);
-   return i;
+         if (n == 0) { return 1.; }
+         if (n == 1) { return value; }
+         double k = value;
+         for (uint64_t i = 1; i < n; i++) {
+                 value *= k;
+         }
+         return value;
  }
 
  uint64_t fact(uint16_t n) {
 
-   if (n == 0) {
-     return 1;
-   }
-   return n * fact(n - 1);
+         uint64_t a = 1;
+         for (uint64_t i = 2; i <= n; i++) {
+                a *= i;
+         }
+         return a;
  }
 
  double calcItem(double x, uint16_t n) {
 
-   double i = pown(x, n) / fact(n);
-   return i;
+         if (n == 0 || x == 0) return 0;
+         return pown(x, n) / fact(n);
  }
 
  double expn(double x, uint16_t count) {
 
-   double exp1 = 1;
-   int z = 1;
-   while (count > z - 1) {
-     exp1 += calcItem(x, z);
-     z++;
-   }
-   return exp1;
+         double a = 1.;
+         for (uint64_t i = 1; i <= count; i++) {
+                 a += pown(x, i) / fact(i);
+         }
+         return a;
  }
 
  double sinn(double x, uint16_t count) {
 
-   double sin1 = 0;
-   for (uint16_t k = 1; k <= count; k++) {
-     sin1 += pown(-1, k - 1) * calcItem(x, (2 * k - 1));
-   }
-   return sin1;
+         double a = x;
+         for (uint64_t i = 2; i <= count; i++) {
+                 a += pown(-1, i - 1) * (pown(x, i * 2 - 1) / fact(i * 2 - 1));
+         }
+         return a;
  }
 
  double cosn(double x, uint16_t count) {
 
-   double cos1 = 0;
-   for (uint16_t k = 1; k <= count; k++) {
-     cos1 += pown(-1, k - 1) * calcItem(x, (2 * k - 2));
-   }
-   return cos1;
+         double a = 1;
+         for (uint64_t i = 2; i <= count; i++) {
+                  a += pown(-1, i - 1) * (pown(x, i * 2 - 2) / fact(i * 2 - 2));
+         }
+         return a;
  }
