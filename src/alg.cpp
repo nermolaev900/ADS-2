d12717ac1bfbb30cbe38946ce8205cea89e010cc
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-05-26 12:08:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,55 +3,57 @@
 #include "alg.h"
 
 
-double pown(double value, uint16_t n) {
+ double pown(double value, uint16_t n) {
 
-         if (n == 0) { return 1.; }
-         if (n == 1) { return value; }
-         double k = value;
-         for (uint64_t i = 1; i < n; i++) {
-                 value *= k;
-         }
-         return value;
+     uint64_t w;
+     double g = 1;
+     for (w = 1; w <= n; w++)
+         g *= value;
+     return g;
  }
 
  uint64_t fact(uint16_t n) {
 
-         uint64_t a = 1;
-         for (uint64_t i = 2; i <= n; i++) {
-                a *= i;
-         }
-         return a;
+     if (n == 0)
+         return 1;
+     return fact(n-1)*n;
  }
 
  double calcItem(double x, uint16_t n) {
 
-         if (n == 0 || x == 0) return 0;
-         return pown(x, n) / fact(n);
+ double calcItem(double g, uint16_t n) {
+     return pown(g, n)/fact(n);
  }
 
  double expn(double x, uint16_t count) {
 
-         double a = 1.;
-         for (uint64_t i = 1; i <= count; i++) {
-                 a += pown(x, i) / fact(i);
-         }
-         return a;
+ double expn(double g, uint16_t count) {
+     uint16_t w;
+     double v;
+     for (w = 0; w <= count; w++) {
+         v += calcItem(g, w);
+     }
+     return v;
  }
 
  double sinn(double x, uint16_t count) {
 
-         double a = x;
-         for (uint64_t i = 2; i <= count; i++) {
-                 a += pown(-1, i - 1) * (pown(x, i * 2 - 1) / fact(i * 2 - 1));
-         }
-         return a;
+ double sinn(double g, uint16_t count) {
+     uint16_t w;
+     double v = 0;
+     for (w = 1; w <= count; w++) {
+         v += pown(-1, w-1)*calcItem(g, 2*w-1);
+     }
+     return v;
  }
 
  double cosn(double x, uint16_t count) {
 
-         double a = 1;
-         for (uint64_t i = 2; i <= count; i++) {
-                  a += pown(-1, i - 1) * (pown(x, i * 2 - 2) / fact(i * 2 - 2));
-         }
-         return a;
+ double cosn(double g, uint16_t count) {
+     uint16_t w;
+     double v = 0;
+     for (w = 1; w <= count; w++) {
+         v += pown(-1, w-1)*calcItem(g, 2*w-2);
+     }
+     return v;
  }
