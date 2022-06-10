11283a5e792c732cbf9507a6e95b6f63e24b09de
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-06-10 17:09:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,50 +1,60 @@
-This commit does not belong to any branch on this repository, and may belong to a fork outside of the repository.
-    return  e;
-}
-double cosn(double x, uint16_t count) {
-    double e = 0;
-    uint64_t c = 1;
-    while (c <= count) {
-        e += pown(-1, c - 1) * pown(x, (2 * c) - 2) / fact((2 * c) - 2);
-        c += 1;
+// Copyright 2022 NNTU-CS
+#include <cstdint>
+#include "alg.h"
+
+
+double pown(double value, uint16_t n) {
+    double osnov = value;
+    for (int i = 1; i < n; i++) {
+        osnov = osnov * value;
     }
-    return  e;
+    return osnov;
 }
+
+uint64_t fact(uint16_t n) {
+    if (n == 0) {
+        return 1;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    uint64_t osnov = 1;
+    for (int i = 2; i <= n; i++) {
+        osnov = osnov * i;
+    }
+    return osnov;
 }
+
+double calcItem(double x, uint16_t n) {
+    return pown(x, n) / fact(n);
+}
+
+double expn(double x, uint16_t count) {
+    double osnov = 0;
+    for (uint16_t i = 0; i <= count; i++) {
+        osnov += calcItem(x, i);
+    }
+    return osnov;
+}
+
 double sinn(double x, uint16_t count) {
-  double k = 0;
-    for (uint16_t i = 1; i <= count; i++) {
-        k = k + (pown(-1, i - 1) * calcItem(x, 2 * i - 1));
+    double osnov = x;
+    for (uint16_t i = 3; i <= 2 * count - 1; i += 4) {
+        osnov -= calcItem(x, i);
     }
-    return k;
-}
-double cosn(double x, uint16_t count) {
-  double k = 0;
-    for (uint16_t i = 1; i <= count; i++) {
-        k = k + (pown(-1, i - 1) * calcItem(x, 2 * i - 2));
+    for (uint16_t i = 5; i <= 2 * count - 1; i += 4) {
+        osnov += calcItem(x, i);
     }
-    return k;
-}
-         a += calcItem(x, n);
-         n++;
-     }
-     return a;
-}
-
-double sinn(double x, uint16_t count) {
-
-   double a = 0;
-     for (uint16_t i = 1; i <= count; i++) {
-         a += pown(-1, (i - 1)) * calcItem(x, (2 * i - 1));
-     }
-     return a;
+    return osnov;
 }
 
 double cosn(double x, uint16_t count) {
-
-   double a = 0;
-     for (uint16_t i = 1; i <= count; i++) {
-         a += pown(-1, (i - 1)) * calcItem(x, (2 * i - 2));
-     }
-     return a;
+    double osnov = 1;
+    for (uint16_t i = 2; i <= 2 * count - 2; i += 4) {
+        osnov -= calcItem(x, i);
+    }
+    for (uint16_t i = 4; i <= 2 * count - 2; i += 4) {
+        osnov += calcItem(x, i);
+    }
+    return osnov;
 }
