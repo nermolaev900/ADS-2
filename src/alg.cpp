49437aa15b814b973e4cb308e1fd49fdf09c6fe3
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-06-10 17:06:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,39 +1,4 @@
-// Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
-double pown(double value, uint16_t n) {
-    double a = 1;
-    for (int i = 1; i <= n; i++) {
-        a = a * value;
-    }
-    return a;
-}
-uint64_t fact(uint16_t n) {
-    uint64_t a = 1;
-    while (n != 0) {
-        a = a * n;
-        n--;
-    }
-    return a;
-}
-double calcItem(double x, uint16_t n) {
-    double a = pown(x, n) / fact(n);
-    return a;
-}
- double expn(double x, uint16_t count) {
-     double e = 0;
-     uint64_t c = 0;
-     while (c <= count) {
-         e += pown(x, c) / fact(c);
-         c += 1;
-     }
-    return  e;
-}
-double sinn(double x, uint16_t count) {
-    double e = 0;
-    for (uint16_t c = 1; c <= count; c++) {
-        e += pown(-1, c-1) * (pown(x, (2 * c)-1) / fact((2 * c) - 1));
-    }
+This commit does not belong to any branch on this repository, and may belong to a fork outside of the repository.
     return  e;
 }
 double cosn(double x, uint16_t count) {
